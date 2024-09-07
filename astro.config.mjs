5a9732f0c47ee324f25b995357e7011142b4a485
aaronoropeza <aoropeza@uwm.edu>
2024-09-07 09:12:34 -0500

    got everything working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Importing both tailwind and react integrations
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    react() // Add the react integration here
+  ],
 });
